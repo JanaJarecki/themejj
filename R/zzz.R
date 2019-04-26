a1e6be69d8df06edf77de2d89fb8e131645beed9
Jana Jarecki <jarecki.jana@gmail.com>
2019-04-26 16:41:03 +0200

    updates zzz.R, works on windows

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,16 @@
 # -----------------------------------------------------------------------------
 # Functions that are run when loading the package
 # -----------------------------------------------------------------------------
-.onLoad <- function() {
-  if( !any(grepl("Roboto Condensed", extrafont::fonts())) )
-  {
-    extrafont::font_import(pattern="Roboto Condensed")
-    pdfFonts <- grDevices::pdfFonts
-    extrafont::loadfonts("pdf")
+.onLoad <- function(libname, pkgname) {
+    if (Sys.info()[1] == "Linux") {
+      dir.create('~/.fonts')
+      file.copy("inst/extdata/fonts/RobotoCondensed-Regular.ttf", "~/.fonts")
+      system('fc-cache -f ~/.fonts')
+    }
+  if (Sys.info()[1] == "Windows") {
+    windowsFonts()
+    extrafont::font_import(pattern = "RobotoCondensed", prompt = FALSE)
+    extrafont::loadfonts(device = "win")
+    windowsFonts()
   }
 }
