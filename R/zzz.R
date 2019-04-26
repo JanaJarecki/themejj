aaee8ab942885b54bf2afdc4473f4513394bf91f
Jana Jarecki <jarecki.jana@gmail.com>
2019-04-26 16:20:13 +0200

    uncomments file

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 # -----------------------------------------------------------------------------
 # Functions that are run when loading the package
 # -----------------------------------------------------------------------------
-# .onLoad <- function() {
-#   if( !any(grepl("Roboto Condensed", extrafont::fonts())) )
-#   {
-#     extrafont::font_import(pattern="Roboto Condensed")
-#     pdfFonts <- grDevices::pdfFonts
-#     extrafont::loadfonts("pdf")
-#   }
-# }
+.onLoad <- function() {
+  if( !any(grepl("Roboto Condensed", extrafont::fonts())) )
+  {
+    extrafont::font_import(pattern="Roboto Condensed")
+    pdfFonts <- grDevices::pdfFonts
+    extrafont::loadfonts("pdf")
+  }
+}
